ca9fe1c8bbc4053e6010d8c4d5a073ed75b527eb
Lucas Duarte <lucascosta12367@gmail.com>
2018-10-26 08:01:20 -0300

    ----

---
+++ banco/repositorio.sql
@@ -0,0 +1,26 @@
+create database repositorio;
+
+create table cadastro(
+	id int auto_increment primary key, 
+	nome varchar(200) not null,
+    senha varchar(16) not null,
+    nascimento date not null,
+	sexo varchar(10)
+    
+);
+
+create table games(
+	id int auto_increment primary key,
+    novogame text,
+    nome varchar(200) not null,
+    descricao text
+
+);
+
+create table materia(
+	id int auto_increment primary key,
+    conteudo text not null,
+	nomej text,
+    foreign key (nome) references games (nome) 
+);
+
